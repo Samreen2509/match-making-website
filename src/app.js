d50f11f88fbb5718bb9f33c1c25b9923c2971922
Samreen2509 <akhtarsamreen1@gmail.com>
2025-01-29 18:36:52 +0530

    db init

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 const express = require("express");
-const {connectDB} = require("./config/database");
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.use(express.json());
+
+app.post("/signup", async(req,res) => {
+const user = new User(req.body);
+try{
+  await user.save();
+res.send("User added successfully...");
+}catch(err){
+  res.status(400).send("Error");
+}
+});
+//feed Api
+app.get("/user", async(req,res) => {
+  const emailid = req.body.email;
+  try{
+    const user = await User.find({email : emailid});  
+    if(user.length===0)
+    {
+      res.send("User not found");
+    }
+    res.send(user);
+  }catch(err){
+    res.status(400).send("Error");
+  }
+  });
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>{
     await mongoose.connect(
-        "mongodb+srv://dating:dating123@cluster0.9kyi3.mongodb.net/"
+        "mongodb+srv://dating:dating123@cluster0.9kyi3.mongodb.net/datingWeb"
     );
 };
 
