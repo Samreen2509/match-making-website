6adf97222bb4ad1a754b34862b1295a9ecf9286e
Samreen2509 <akhtarsamreen1@gmail.com>
2025-01-31 23:57:36 +0530

    login/api created

--- src/app.js
+++ src/app.js
@@ -2,16 +2,44 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async(req,res) => {
-const user = new User(req.body);
+  const {firstName, lastName, email,password} = req.body;
+  //Encrypt the password
+  const passwordHash = await bcrypt.hash(password,10);
+  
+const user = new User({firstName, lastName, email,password : passwordHash});
 try{
   await user.save();
-res.send("User added successfully...");
+  res.send("User added successfully...");
 }catch(err){
-  res.status(400).send("Some required field are missing");
+  res.status(400).send("Some required field are missing" + err.message);
+}
+});
+
+//login api 
+app.post("/login", async(req,res) =>{
+  try{
+    const {password,email} = req.body;
+
+    const user = await User.findOne({email : email});
+    if(!user){
+      throw new Error("Invalid credential");
+    }
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+    if(isPasswordValid)
+    {
+      res.send("User login successfully");
+    }
+    else {
+      res.send("Invalid credential");
+    }
+
+  }catch(err){
+  res.status(400).send("ERROR : " + err.message);
 }
 });
 //feed Api
@@ -25,7 +53,7 @@ app.get("/user", async(req,res) => {
     }
     res.send(user);
   }catch(err){
-    res.status(400).send("Error");
+    res.status(400).send("Error" + err.message);
   }
   });
 
@@ -35,7 +63,7 @@ app.get("/user", async(req,res) => {
       const user = await User.find({});
       res.send(user)
     }catch(err){
-      res.status(400).send("Error");
+      res.status(400).send("Error" + err.message);
     }
   });
 
@@ -46,7 +74,7 @@ app.get("/user", async(req,res) => {
       const user = await User.findByIdAndDelete({ _id : userId });
       res.send("User deleted successfully");
      }catch(err){
-      res.status(400).send("Error");
+      res.status(400).send("Error" + err.message);
     }
   });
 
@@ -65,7 +93,7 @@ app.patch("/user", async(req,res) => {
     await User.findByIdAndUpdate({_id : userId}, data,{ runValidators : true,});
     res.send("User updated successfully");
     }catch(err){
-      res.status(400).send("Update failed");
+      res.status(400).send("Update failed" + err.message);
     }
 });
 
